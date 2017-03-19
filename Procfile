afe4423eb438a4b4b990ddad2ffa15ec07e6bc92
Walter Alonso <wj.alonso@uniandes.edu.co>
2017-03-19 18:51:40 -0500

    included pages to heroku

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: java -jar target/Ecos_CalculoTamanioRelativo-1.0-SNAPSHOT.jar "ArchivoCargaLocMetodo.txt"
+web: java -cp target/Ecos_CalculoTamanioRelativo-1.0-SNAPSHOT.jar uniandes.ecos.conceptosAvanzados.tamanioRelativo.controlador.CalculoTamanioRelativoWeb "ArchivoCargaLocMetodo.txt"
---
+++ src/main/java/uniandes/ecos/conceptosAvanzados/tamanioRelativo/controlador/CalculoTamanioRelativoWeb.java
@@ -0,0 +1,109 @@
+/**
+ * Proposito:     Calcular el tamanio relativo de varias partes.
+ * Autor(s):   Walter Javier Alonso Roa
+ * Fecha creacion: 19 Marzo 2017
+ * Modificado por: Walter Alonso 
+ * Ultima modificacion: 19 Marzo 2017
+*/
+
+package uniandes.ecos.conceptosAvanzados.tamanioRelativo.controlador;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+import uniandes.ecos.conceptosAvanzados.tamanioRelativo.Modelo.ArchivoCategoriaFuncional;
+import uniandes.ecos.conceptosAvanzados.tamanioRelativo.Modelo.CategoriaFuncional;
+import uniandes.ecos.conceptosAvanzados.tamanioRelativo.Modelo.TipoCategoria;
+import static spark.Spark.*;
+import spark.ModelAndView;
+import spark.template.freemarker.FreeMarkerEngine;
+
+/**
+ * Clase principal que se encarga de controlas las peticiones web del usuario.
+ * 
+ * @author WALONSO
+ *
+ */
+public class CalculoTamanioRelativoWeb {
+
+	/**
+	 * Main metodo principal, recibe del usuario la informacion y de acuerdo a
+	 * ello, orquesta para mostrar resultados.
+	 * 
+	 * @param args:
+	 *            argmentos del sistema
+	 */
+	public static void main(String[] args) {
+		
+		//Request al home del sitio.
+		get("/", (req, res) -> {
+			Map<String, Object> attributes = new HashMap<>();
+			try {
+				String archivoLoc = "./ArchivoProcesar/ArchivoCargaLocMetodo.txt";
+				procesarArchivo(attributes, TipoCategoria.Clase, archivoLoc, "LOC/Method Data", "TipoCategoriaLoc");
+
+				String archivoCapitulos = "./ArchivoProcesar/ArchivoCargaLocCapitulos.txt";
+				procesarArchivo(attributes, TipoCategoria.Capitulo, archivoCapitulos, "Pgs/Chapter",
+						"TipoCategoriaCapitulo");
+
+				return new ModelAndView(attributes, "calculoTamanioRelativo.ftl");
+			} catch (Exception ex) {
+				attributes.put("message", ex.getMessage());
+				return new ModelAndView(attributes, "error.ftl");
+			}
+		}, new FreeMarkerEngine());
+
+		// Request para ver las pruebas de archivo
+		get("/PruebasArchivo", (request, response) -> {
+			Map<String, Object> attributes = new HashMap<>();
+			try {
+				String archivo = "./ArchivoProcesar/ArchivoExtensionErronea.dat";
+				ArchivoCategoriaFuncional archivoCategoriaFuncional = new ArchivoCategoriaFuncional(TipoCategoria.Clase,
+						archivo);
+				archivoCategoriaFuncional.procesarArchivo();
+			} catch (Exception ex) {
+				attributes.put("ExtensionErronea", ex.getMessage());
+				attributes.put("MensajeEsperadoExtensionErronea", "El archivo debe ser en formato txt");
+			}
+
+			try {
+				String archivo = "./ArchivoProcesar/ArchivoVacio.txt";
+				ArchivoCategoriaFuncional archivoCategoriaFuncional = new ArchivoCategoriaFuncional(TipoCategoria.Clase,
+						archivo);
+				archivoCategoriaFuncional.procesarArchivo();
+			} catch (Exception ex) {
+				attributes.put("ArchivoVacio", ex.getMessage());
+				attributes.put("MensajeEsperadoArchivoVacio", "El archivo esta vacio");
+			}
+
+			try {
+				String archivo = "./ArchivoProcesar/ArchivoNoExiste.txt";
+				ArchivoCategoriaFuncional archivoCategoriaFuncional = new ArchivoCategoriaFuncional(TipoCategoria.Clase,
+						archivo);
+				archivoCategoriaFuncional.procesarArchivo();
+			} catch (Exception ex) {
+				attributes.put("ArchivoNoExiste", ex.getMessage());
+				attributes.put("MensajeEsperadoArchivoNoExiste", "No hay archivo que cargar");
+			}
+
+			return new ModelAndView(attributes, "testArchivo.ftl");
+		}, new FreeMarkerEngine());
+	}
+
+	private static void procesarArchivo(Map<String, Object> attributes, TipoCategoria tipoCategoria, String archivo,
+			String nombreCategoria, String llave) throws Exception {
+		ArchivoCategoriaFuncional archivoCategoriaFuncional = new ArchivoCategoriaFuncional(tipoCategoria, archivo);
+		archivoCategoriaFuncional.procesarArchivo();
+		ArrayList<Double> contenidoArchivo = archivoCategoriaFuncional.darDatos();
+		CategoriaFuncional categoriaFuncional = new CategoriaFuncional(contenidoArchivo, nombreCategoria);
+		categoriaFuncional.calcularTamanios();
+
+		attributes.put(llave, categoriaFuncional.darNombre());
+		attributes.put(llave + "VS", categoriaFuncional.darTamanioVerySmall().darCantidad());
+		attributes.put(llave + "S", categoriaFuncional.darTamanioSmall().darCantidad());
+		attributes.put(llave + "M", categoriaFuncional.darTamanioMedium().darCantidad());
+		attributes.put(llave + "L", categoriaFuncional.darTamanioLarge().darCantidad());
+		attributes.put(llave + "VL", categoriaFuncional.darTamanioVeryLarge().darCantidad());
+	}
+}
